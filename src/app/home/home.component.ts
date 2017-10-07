eb2ecbe5f49ec763283ba92641807193ae3c3102
Praveen Kumar Guntoju <gpravin999@gmail.com>
2017-10-07 20:54:02 +0530

    Deleting  unwanted code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,20 +14,13 @@ import { DropdownComponent } from '../dropdown/dropdown/dropdown.component';
 })
 export class HomeComponent implements OnInit {
 
-  API = 'http://localhost:3000';
-  path = 'app/jsonfiles/makes.json';
+//   API = 'http://localhost:3000';
+//   path = 'app/jsonfiles/makes.json';
   people: any[] = [];
-  selectedCar: any;
-  car: any[];
-  modelPath: any;
-  ModelCars: any[];
-  selectedModel: any;
-  selectedValue: boolean;
-  setPlaceHolder: boolean;
+ 
 
   ngOnInit(){
     this.getAllPeople(this.people);
-    // this.getCars();
   }
   constructor(private http: Http, private router: Router) {}
    onClickMe(eve){
@@ -35,32 +28,6 @@ export class HomeComponent implements OnInit {
      debugger;
      this.router.navigate(['details', eve.CARNUM], {queryParams: {'qdata': 200}, preserveQueryParams: true});
    }
-//     onSelectCar(selectedCar){
-//       debugger;
-//       this.selectedModel = {};
-//       this.selectedValue = true;
-//       this.setPlaceHolder = true;
-//       if (selectedCar.make === 'ford'){
-//        this.modelPath =  'app/jsonfiles/ford.json';
-//       } else{
-//         this.modelPath = 'app/jsonfiles/acura.json';
-//        }
-//  const headers = new Headers({ 'Content-Type': 'application/json' });
-//     const options = new RequestOptions({ headers: headers});
-//     this.http.get(`${this.modelPath}`, options)
-//       .map(res => res.json())
-//       .subscribe(data => {
-//         console.log(data);
-//         this.ModelCars = data.models;
-//       });
-
-
-//     };
-//    onSelectModel(selectedCar){
-//      debugger;
-//     this.selectedModel = selectedCar;
-//     this.setPlaceHolder = false;
-//    };
 
     onRegister(eve){
      // tslint:disable-next-line:no-debugger
@@ -72,23 +39,11 @@ export class HomeComponent implements OnInit {
     const headers = new Headers({ 'Content-Type': 'application/json' });
     const body = {'action': 'getData'};
     const options = new RequestOptions({ headers: headers});
-    this.http.post(`${this.API}/app`, body, options)
+    this.http.post(`app`, body, options)
       .map(res => res.json())
       .subscribe(data => {
         console.log(data);
         this.people = data.driverData;
       });
   };
-
-  //  getCars() {
-  //   const headers = new Headers({ 'Content-Type': 'application/json' });
-  //   const options = new RequestOptions({ headers: headers});
-  //   this.http.get(`${this.path}`, options)
-  //     .map(res => res.json())
-  //     .subscribe(data => {
-  //       console.log(data);
-  //       this.car = data;
-  //     });
-  // };
-
 }
