b5fbe42bcdf7c8ee75e82127a67b937350b707a9
Praveen Kumar Guntoju <gpravin999@gmail.com>
2017-10-12 22:48:24 +0530

    Test

--- server.js
+++ server.js
@@ -143,7 +143,8 @@ mongodb.MongoClient.connect('mongodb://chintu:chintu123@ds161164.mlab.com:61164/
         console.log("ERROR: " + reason);
          res.status(code || 500).json({"error": message});
     } else {
-     res.status(200).json({data:docs});
+     res.status(200);
+     res.send({data: docs});
     }
   });
 });
