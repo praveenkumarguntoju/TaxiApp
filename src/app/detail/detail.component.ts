fcc00aba1ebd3fe03bcd687dff936491807d2631
Praveen Kumar Guntoju <gpravin999@gmail.com>
2017-10-14 00:46:25 +0530

    Test Datas

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -22,7 +22,8 @@ export class DetailComponent implements OnInit {
   constructor(private actRoute: ActivatedRoute,private http: Http, private router: Router) {
     // tslint:disable-next-line:no-debugger
     debugger;
-      this.subscription = this.router.routerState.queryParams.subscribe(params => {
+      this.subscription = this.router.routerState
+                            .queryParams.subscribe(params => {
          debugger;
         this.id =  params['idData'];
       });
