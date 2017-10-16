f9a42fbede5a539db077a3a17186b53668152387
Praveen Kumar Guntoju <gpravin999@gmail.com>
2017-10-16 17:15:34 +0530

    Change

--- server.js
+++ server.js
@@ -166,7 +166,7 @@ mongodb.MongoClient.connect('mongodb://chintu:chintu123@ds161164.mlab.com:61164/
 app.put("/app/contacts/:id", function(req, res) {
       var updateDoc = req.body;
       delete updateDoc._id;
-   db.collection("drivers").updateOne({_id: ObjectID(req.params.id)}, updateDoc, function(err, doc) {
+   db.collection("drivers").updateOne({_id:  ObjectId(req.params.id)}, updateDoc, function(err, doc) {
     if (err) {
       handleError(res, err.message, "Failed to update contact");
     } else {
