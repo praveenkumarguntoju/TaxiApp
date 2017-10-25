af0a407c142bc60bafb735eeb51d41721aa203fc
Praveen Kumar Guntoju <gpravin999@gmail.com>
2017-10-25 17:46:13 +0530

    Test

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -28,7 +28,7 @@ export class HomeComponent implements OnInit {
  
 
   ngOnInit(){
-     document.getElementById("loading-image").show();
+//      document.getElementById("loading-image").show();
     this.getAllPeople(this.people);
 
   }
@@ -80,7 +80,7 @@ export class HomeComponent implements OnInit {
     this.http.get('/app')
                  .toPromise().then((response)=>{
                 this.dataGet(response);
-                 document.getElementById("loading-image").hide();
+//                  document.getElementById("loading-image").hide();
        }).catch(this.handleError); 
   };
   
