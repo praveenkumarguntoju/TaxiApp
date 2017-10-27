a5ae5a1f8455123c3b0fdc43af18a0ca2c17d625
Praveen Kumar Guntoju <gpravin999@gmail.com>
2017-10-27 12:54:09 +0530

    save

--- server.js
+++ server.js
@@ -180,7 +180,8 @@ mongodb.MongoClient.connect('mongodb://chintu:chintu123@ds229415.mlab.com:29415/
 });
   
 app.get("/app/comments/:id", function(req, res) {
-    db.collection("commentsData").find({userId: req.params.id},function(err, docs){
+  
+    db.collection("commentsData").find({}).toArray(function(err, docs) {
     if (err) {
         console.log("ERROR: " + reason);
          res.status(code || 500).json({"error": message});
@@ -189,6 +190,8 @@ app.get("/app/comments/:id", function(req, res) {
      res.send({data: docs});
     }
   });
+   
+  
 });
   
   
