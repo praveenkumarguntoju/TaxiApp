d4688f6180c3188a276c813f04767009ebd8aa5b
Praveen Kumar Guntoju <gpravin999@gmail.com>
2017-10-27 12:59:33 +0530

    Test

--- server.js
+++ server.js
@@ -181,7 +181,7 @@ mongodb.MongoClient.connect('mongodb://chintu:chintu123@ds229415.mlab.com:29415/
   
 app.get("/app/comments/:id", function(req, res) {
   
-    db.collection("commentsData").find({}).toArray(function(err, docs) {
+    db.collection("commentsData").find({userId: req.params.id}).toArray(function(err, docs) {
     if (err) {
         console.log("ERROR: " + reason);
          res.status(code || 500).json({"error": message});
