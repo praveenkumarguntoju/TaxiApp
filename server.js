67911440f5786d820e3b1206ade45239a2fe9877
praveenkumarguntoju <gpravin999@gmail.com>
2017-11-29 16:13:59 +0530

    new Data

--- server.js
+++ server.js
@@ -360,6 +360,18 @@ app.post("/app/usermapdata", function(req, res) {
   });
 });
 
+app.get("/app/usergetmapdata/:id", function(req, res) {
+  db.collection("userMapData").find({userId: req.params.id}).toArray(function(err, docs) {
+    if (err) {
+        console.log("ERROR: " + reason);
+         res.status(code || 500).json({"error": message});
+    } else {
+     res.status(200);
+     res.send({data: docs});
+    }
+  });
+});
+
 
 
 });
