83a63d6e03dd14a2ccc87ac376263eacf9f1f9f7
praveenkumarguntoju <gpravin999@gmail.com>
2017-12-12 19:48:02 +0530

    test

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -64,14 +64,14 @@ export class DetailComponent implements OnInit {
                     this.driverDetails = contacts.data;
                     this.imageUrl = contacts.data.picFile;
                     if(contacts.data.lat){
-                      var myLatLng = new google.maps.LatLng(contacts.data.lat,contacts.data.lng),
+                      var myLatLng = new google.maps.LatLng(contacts.data.lat,contacts.data.lng);
                       this.directionsService = new google.maps.DirectionsService();
                       this.directionsDisplay = new google.maps.DirectionsRenderer();
                       this.map = new google.maps.Map(document.getElementById('map'), {
                         zoom: 8,
                         center: myLatLng 
                       });
-                     this.directionsDisplay.setMap(this.map);
+                    this.directionsDisplay.setMap(this.map);
                     var marker = new google.maps.Marker({
                       position: myLatLng,
                       map: this.map
