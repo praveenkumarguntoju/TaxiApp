ca27cd8101729ad3eb63e788578298b65b540724
Praveen Kumar Guntoju <gpravin999@gmail.com>
2020-07-15 00:10:28 +0100

    Update server.js

--- server.js
+++ server.js
@@ -226,6 +226,7 @@ app.post("/app/registeruser", function(req, res) {
   console.log(newContact.email);
   var transporter = nodemailer.createTransport({
     service: 'gmail',
+    secure: false,
     auth: {
       user: 'booktaxiready@gmail.com',
       pass: 'chintu1234'
